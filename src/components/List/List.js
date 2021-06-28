59f12bcb24534d3b1147f6b8fd20012c8c76ece8
pkirill <poletaev@ucoz-team.net>
2021-06-28 20:08:50 +0300

    refactor: Изменит логику закрытия списка

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -17,11 +17,15 @@ export default function List(props) {
     setList(listArray.filter(o => !o.parentId))
   }, [])
 
+  /**
+   * @param id
+   */
   function toggle({id}) {
     const index = list.findIndex(item => item.id === id)
 
     if (!active.includes(id)) {
       const children = Object.values(props.items || {}).filter(o => o.parentId === id)
+
       setActive(prev => [...prev, id])
       setList(prev => {
         return [
@@ -32,34 +36,30 @@ export default function List(props) {
       })
 
     } else {
-      // Ужасная реализация, переписать на один обход
-      const tempArray = []
-      let l = function (_id) {
-        let result = list.reduce((res, o) => {
-          if (o.parentId === _id) { // находим активных дочек
-            res.push(o.parentId)
-          }
+      /**
+       * @param _id
+       * @param count
+       * @returns Number
+       */
+      const countActiveChildById = (_id, count = 0) => list.reduce((tempCount, item) => {
+        if (item.parentId === _id) tempCount += 1
+        if (active.includes(item.id) && item.parentId === _id) return countActiveChildById(item.id, tempCount)
+        return tempCount
+      }, count)
 
-          if (active.includes(o.id) && o.parentId === _id) { // спускаемся вниз по ветке
-            l(o.id)
-          }
-
-          return res
-        }, tempArray)
-
-        return result
-      }
-      const activeTreeNode = l(id)
-
-      list.splice(index + 1, activeTreeNode.length)
-
-      setList([...list])
-
-      new Set(activeTreeNode).forEach(i => {
-        active.splice(active.indexOf(i), 1)
+      // Почему то я не могу получить обновленный  list после setList. По этому использую временную переменную
+      let tempList = [];
+      setList(prev => {
+        tempList = [
+          ...prev.slice(0, index + 1),
+          ...prev.slice(index + 1 + countActiveChildById(id), list.length)
+        ];
+        return tempList
       })
-
-      setActive(active)
+      // а еще и костыль с таймаутом. Навеное есть какой то хук?
+      setTimeout(_ => {
+        setActive(active.filter(i => i !== id && Array.from(tempList, o => o.id).includes(i)))
+      },1)
     }
   }
 
@@ -71,7 +71,6 @@ export default function List(props) {
       {list.map((item, i) => {
         //@todo - почему убирая key={i.toString()} ререндерится вообще весь список?????? Весь прикол в ключах? нужно больше ключей богу ключей?
         //  если так оно и есть, можно ли как то генеировать ключи аля: {item.id+'childComponents1'}, найти примеры.
-        console.log(item.anchors , item.title)
         const children = Object.values(props.items || {}).filter(o => o.parentId === item.id)
         return <ListItem key={i.toString()} active={active.includes(item.id)} click={toggle}
                          lastItem={!!children.length} item={item}/>
