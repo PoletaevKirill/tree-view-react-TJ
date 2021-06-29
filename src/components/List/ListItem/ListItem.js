7cf7016089dbd9ba3f4e7bdab9a1b0ea8af28442
Кирилл Полетаев <poletaev@ucoz-team.net>
2021-06-29 14:47:52 +0300

    feat: Подключит шрифты, обновит стрелочку и ее стили

--- src/components/List/ListItem/ListItem.js
+++ src/components/List/ListItem/ListItem.js
@@ -1,4 +1,4 @@
-import ArrowList from '../../../img/arrow-list.svg'
+import Right from '../../../img/right.svg'
 import {StyledLi, Arrow} from './styles'
 import store from "../../../store";
 
@@ -9,36 +9,25 @@ import store from "../../../store";
  * @constructor
  */
 export default function ListItem({item, click, clickLastChild, active, activeLink, lastItem = false}) {
-  const activeClass =  (active) ? 'active' : null
+  const activeClass = (active) ? 'active' : null
   const styleLeftMargin = {marginLeft: item.level ? 16 * item.level + 'px' : null}
 
 
   return <StyledLi key={item.id}
                    style={styleLeftMargin}>
     {
-      lastItem &&
-      <div className={activeClass} onClick={() => click(item)}>
-        <Arrow>
-          <img src={ArrowList} alt="React Logo" width="10" height="10"
-               style={{display: 'inline-block', transition: '.3s ease'}}/>
-        </Arrow>
+      <div className={`pt-2 pb-2 ${activeClass}`} onClick={() => click(item)} style={{"position": "relative"}}>
+        {lastItem && <Arrow src={Right} alt="React Logo" width="10" height="10"/>}
         <span>{item.title}</span>
       </div>
     }
     {
-      !lastItem &&
-      <div onClick={()=>clickLastChild(item)}>
-        <div>{item.title}</div>
-        {
-          activeLink === item.id && item.anchors.map((anc, i) => {
-            const anchors = store['anchors'][anc]
-
-            return <div key={anchors.id} className="pt-2 pb-2">
-              <a  href={anchors.anchor}>{anchors.title}</a>
-            </div>
-          })
-        }
-      </div>
+      active && item.anchors && item.anchors.map((anc, i) => {
+        const anchors = store['anchors'][anc]
+        return <div key={anchors.id} className="pt-2 pb-2">
+          <a href={anchors.anchor}>{anchors.title}</a>
+        </div>
+      })
     }
   </StyledLi>;
 }
--- src/components/List/ListItem/styles.js
+++ src/components/List/ListItem/styles.js
@@ -3,15 +3,16 @@ import styled from "styled-components";
 const StyledLi = styled.li`
   font-size: 14px;
   position: relative;
-  padding: 8px 0 8px 16px;
 `
-const Arrow = styled.span`
+const Arrow = styled.img`
   position: absolute;
-  left: 8px;
+  left: -10px;
   display: inline-block;
   width: 8px;
-  top: 8px;
-  height: 0;
+  top: 50%;
+  height: 8px;
+  transform: translateY(-50%) rotate(90deg);
+  transition: transform .3s ease;
 `
 
 export  {StyledLi, Arrow}
